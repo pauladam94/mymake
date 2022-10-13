1d7b984b30a0037608b0264f2861f7f3c5a5f589
TI64CLi <neuronix9302@gmail.com>
2022-10-13 14:51:58 +0200

    Bug fixes

--- debug.h
+++ debug.h
@@ -1,6 +1,9 @@
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
-void debug_error(char *, char*, ...);
+#define debug_error(c, f, ...) { \
+        fprintf(stderr, "An unexpected error occured in function '%s' :\n", c); \
+        fprintf(stderr, f, __VA_ARGS__); \
+    }
 
 #endif
--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "graph.h"
-#include "rule.h"
 
-
-dependencieGraph_t *creation_graphe() {
-
-}
-
-
+#include "debug.h"
+//#include "graph.h"
+//#include "rule.h"
 
 int main(int argc, char *argv[]) {
+
+  debug_error("main", "Il y a %d arguments pour le programme '%s'\n", argc, argv[0]);
   // 1er étape création du graphe
 
   // 2eme étape vétification de cycle
 
   // 3eme étape exécution des lignes en fonction des dates de sauvegardes
+
+  return EXIT_SUCCESS;
 }
